8f603c6b531403f585873328307b03000d170644
fabaronr <fabaronr@unal.edu.co>
2014-05-25 08:31:40 -0500

    final commit. Functional Program

--- cachematrix.R
+++ cachematrix.R
@@ -6,13 +6,13 @@
 makeCacheMatrix <- function(x = matrix()) {
   require("MASS")
   inverse <- NULL
-  set <- function(y) {
+  set <- function(y) #set the value of the matrix{
     x <<- y
     inverse <<- NULL
   }
-  get <- function() x
-  setinverse <- function(ginv) inverse <<- ginv
-  getinverse <- function() inverse
+  get <- function() x # get the value of the matrix
+  setinverse <- function(ginv) inverse <<- ginv #set the value of the matrix inverse
+  getinverse <- function() inverse              #get the value of the matrix
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -23,9 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  require("MASS")
+  require("MASS") # This package is Required to make the calculations
   -        inverse <- x$setinverse
-  +        inverse <- x$getinverse()
+  +        inverse <- x$getinverse() ## get the inverse if it has been calculated before
   if(!is.null(inverse)) {
     message("getting cached data")
     return(inverse)
